b0610bcb53ee46669e9239ce4a049a88c84affe0
Harshil Borthakur <ghouldrago0@gmail.com>
2021-02-21 17:43:16 +0530

    init

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using ReactiveUI;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace SimpleTextEditor.ViewModels
+{
+    
+    public class MainWindowViewModel : ViewModelBase
+    {
+        
+
+        public void Save()
+        {
+            File.WriteAllTextAsync(@"Doc.txt", Text);
+        }
+        public void Load()
+        {
+            Text = File.ReadAllTextAsync(@"Doc.txt", Encoding.UTF8).Result;
+            
+
+        }
+        public string? Text{get; set;}
+    }
+}
