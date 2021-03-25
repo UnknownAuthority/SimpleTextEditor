883f01fea655c295da7540045b4cf4f0702eaa63
Harshil <ghouldrago0@gmail.com>
2021-03-25 10:18:33 +0530

    ig

--- ViewModels/TextEditViewModel.cs
+++ ViewModels/TextEditViewModel.cs
@@ -6,12 +6,15 @@ namespace SimpleTextEditor.ViewModels
 {
     public class TexteditViewModel : ViewModelBase
     {
-        public string? text;
-        public string? Text
+
+
+        public string text;
+        public string Text
         {
             get => text;
             //  Again using RaiseAndSetIfChanged because Avalonia depends on change notifs to update gui
             set => this.RaiseAndSetIfChanged(ref text, value);
+
         }
 
         public void Save() => File.WriteAllTextAsync(@"Doc.txt", Text);
@@ -20,17 +23,14 @@ namespace SimpleTextEditor.ViewModels
 
             if (File.Exists(@"Doc.txt"))
             {
+
                 Text = File.ReadAllTextAsync(@"Doc.txt", Encoding.UTF8).Result;
+
             }
             else
             {
                 File.Create(@"Doc.txt");
             }
-
-
-
-
-
         }
     }
 }
