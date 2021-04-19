a872ca06d7f8a7a274970bc779adfb17a0ff95a4
Harshil <ghouldrago0@gmail.com>
2021-04-19 16:03:41 +0530

    Just some small stuff

--- ViewModels/FilePathViewModel.cs
+++ ViewModels/FilePathViewModel.cs
@@ -3,23 +3,22 @@ namespace SimpleTextEditor.ViewModels
 {
     public class FilePathViewModel : ViewModelBase
     {
-        public static string? path{get; set;}
+        public static string? path { get; set; }
         public static string Path
         {
             get
             {
-
-
                 if (path == null || path.Length < 1 || !System.IO.Directory.Exists(path))
                 {
                     return "Doc.txt";
+                    
                 }
                 return path;
             }
 
             set => path = value;
         }
-    }    
-         
+    }
+
 
 }
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -6,29 +6,25 @@ using System.IO;*/
 
 namespace SimpleTextEditor.ViewModels
 {
-    
+
     public class MainWindowViewModel : ViewModelBase
     {
         public ViewModelBase? content;
-        public MainWindowViewModel(){
+        public MainWindowViewModel() =>
             //  We are setting the Content to a new instance to the TexteditViewModel
-            Content =  new TextEditViewModel();
+            Content = new TextEditViewModel();
+        public ViewModelBase? Content
+        {
 
-        }
-        public ViewModelBase? Content{
-            
             get => content;
             //  using RaiseAndSetIfChanged because avalonaia uses change notifs to Update the gui
             //  this will be useful later when we will be able to switch between multiple windows 
             set => this.RaiseAndSetIfChanged(ref content, value);
         }
-        public void FilePath()
-        {
-            Content = new FilePathViewModel();
-        }  
-        public void TextEdit() => Content = new TextEditViewModel(); 
-        
-        
-        
-    }   
+        public void FilePath() => Content = new FilePathViewModel();
+        public void TextEdit() => Content = new TextEditViewModel();
+
+
+
+    }
 }
